c1aeb8726223594962f19be90fce91e67b78b290
GigaPrime <noname_mailuser@ukr.net>
2019-02-26 01:05:54 +0200

    Add files via upload

--- Constants.h
+++ Constants.h
@@ -1,5 +1,14 @@
 #pragma once
 
+// Employee
 const int MAX_FIRST_NAME_LENGHT = 20;
 const int MAX_LAST_NAME_LENGHT = 20;
 const int MAX_PHONE_LENGHT = 14;
+// Menu
+const int MAX_MENU_ITEMS = 8;
+// DataBase
+const int INITIAL_SIZE = 10;
+const int INCREASE_COEF = 2;
+const char EMPTY_RECORD[] = "~Empty record~";
+const char OUT_OF_RANGE[] = "~Out of range~";
+
